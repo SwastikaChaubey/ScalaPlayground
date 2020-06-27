0f25df6ceae0be3b45e6efc4fd0e385128fb88e9
SwastikaChaubey <swastikachaubey2122@gmail.com>
2020-06-27 19:51:23 +0530

    Question 2 added

---
+++ src/main/scala/com/Swastika/scala/Main.scala
@@ -0,0 +1,9 @@
+package com.Swastika.scala
+
+object Main {
+  def main(args: Array[String]): Unit = {
+   // HelloWorld.display()
+    var ob1 = new Question2
+    ob1.func(5)
+  }
+}
---
+++ src/main/scala/com/Swastika/scala/Question2.scala
@@ -0,0 +1,9 @@
+package com.Swastika.scala
+
+class Question2 {
+
+  def func(n: Int): Unit = println("Hello World!\n" *n)
+
+  //var func = (n: Int) => println("Hello World!\n" *n)
+  //def func(n: Int) = for( i <- 1 to n) println("Hello World!")
+}
